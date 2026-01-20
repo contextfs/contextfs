1e27679ec4425c470c982fbd52b998bfaf0fe9f8
MagnetonIO <mlong@magneton.io>
2026-01-19 23:07:01 -0600

    Bump version to 0.2.27

--- src/contextfs/__init__.py
+++ src/contextfs/__init__.py
@@ -34,7 +34,7 @@ Graph Example:
     merged = lineage.merge(["mem1", "mem2"], strategy="union")
 """
 
-__version__ = "0.2.26"
+__version__ = "0.2.27"
 
 from contextfs.core import ContextFS
 from contextfs.memory_lineage import ConflictResolution, MemoryLineage, MergeStrategy
